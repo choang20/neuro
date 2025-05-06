7daff4b825005329bb816982f2bcd388057de99e
Max Taggart <max.taggart@healthcatalyst.com>
2025-05-05 20:59:14 -0600

    Initial Commit

---
+++ Neurocular/NeurocularApp.swift
@@ -0,0 +1,17 @@
+//
+//  NeurocularApp.swift
+//  Neurocular
+//
+//  Created by Max Taggart on 5/5/25.
+//
+
+import SwiftUI
+
+@main
+struct NeurocularApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
